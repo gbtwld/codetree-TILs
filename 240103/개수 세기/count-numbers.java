43ce3d2629cb1cb7850d10b8fd448cd09aacf4b6
gbtwld <gbtwld@gmail.com>
2024-01-03 18:01:28 +0900

    Title: 개수 세기, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240103/개수 세기/count-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), cnt = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == m) {
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
