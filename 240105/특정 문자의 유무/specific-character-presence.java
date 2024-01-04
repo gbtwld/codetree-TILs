6981201fa0e7c420604ddbda7b5ec0d665812b0f
gbtwld <gbtwld@gmail.com>
2024-01-05 04:10:39 +0900

    Title: 특정 문자의 유무, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 240105/특정 문자의 유무/specific-character-presence.java
+++ 240105/특정 문자의 유무/specific-character-presence.java
@@ -4,17 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
+        boolean exist1 = false, exist2 = false;
 
-        if (str.contains("ee")) {
-            System.out.print("Yes ");
-        } else {
-            System.out.print("No ");
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'e' && str.charAt(i + 1) == 'e') {
+                exist1 = true;
+            }
+            if (str.charAt(i) == 'a' && str.charAt(i + 1) == 'b') {
+                exist2 = true;
+            }
         }
 
-        if (str.contains("ab")) {
-            System.out.print("Yes");
-        } else {
-            System.out.print("No");
-        }
+        System.out.print(exist1 ? "Yes " : "No ");
+        System.out.print(exist2 ? "Yes" : "No");
     }
 }
