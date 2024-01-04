6a660dbf7a57d354710c451944ddc729575937ba
gbtwld <gbtwld@gmail.com>
2024-01-04 20:47:47 +0900

    Title: 파스칼의 삼각형, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240104/파스칼의 삼각형/pascal's-triangle.java
+++ 240104/파스칼의 삼각형/pascal's-triangle.java
@@ -5,11 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] arr = new int[n][n];
-        arr[0][0] = arr[1][0] = arr[1][1] = 1;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i + 1; j++) {
-                if (j == 0) {
+                if (j == 0 || j == i) {
                     arr[i][j] = 1;
                 } else {
                     arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
