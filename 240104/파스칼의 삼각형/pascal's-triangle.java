37fa58959a8f614c04f6100e9786045c63fb84e1
gbtwld <gbtwld@gmail.com>
2024-01-04 20:45:32 +0900

    Title: 파스칼의 삼각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240104/파스칼의 삼각형/pascal's-triangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        arr[0][0] = arr[1][0] = arr[1][1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
