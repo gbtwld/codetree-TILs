da1c1bcfedded2fc762a52d2cc606e67f38eaf5e
gbtwld <gbtwld@gmail.com>
2024-01-05 17:04:03 +0900

    Title: a로 채워넣기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240105/a로 채워넣기/filling-with-a.java
+++ 240105/a로 채워넣기/filling-with-a.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 1. char array
         String str = sc.next();
+        // 1. char array
+        /*
         char[] arr = str.toCharArray();
 
         arr[1] = 'a';
         arr[arr.length - 2] = 'a';
 
         String result = String.valueOf(arr);
+
+        System.out.print(result);
+        */
+        
+        // 2. substring
+        String result = str.substring(0, 1) + 'a' + str.substring(2, str.length() - 2) + 'a' + str.charAt(str.length() - 1);
 
         System.out.print(result);
     }
