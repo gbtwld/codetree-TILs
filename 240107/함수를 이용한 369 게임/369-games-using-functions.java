34d73b64ae9d2ab41ad4569725327e25e963d6f1
gbtwld <gbtwld@gmail.com>
2024-01-07 22:40:05 +0900

    Title: 함수를 이용한 369 게임, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240107/함수를 이용한 369 게임/369-games-using-functions.java
+++ 240107/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean is369(int n) {
-        String str = Integer.toString(n);
-        boolean satisfied = false;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '3' || str.charAt(i) == '6' || str.charAt(i) == '9') {
-                satisfied = true;
-                break;
+    public static boolean is369Num(int n) {
+        while (n > 0) {
+            if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) {
+                return true;
             }
+            n /= 10;
         }
+        return false;
+    }
 
-        return satisfied;
+    public static boolean is369(int n) {
+        return (n % 3 == 0 || is369Num(n));
     }
 
     public static void main(String[] args) {
@@ -20,7 +20,7 @@ public class Main {
         int a = sc.nextInt(), b = sc.nextInt(), cnt = 0;
 
         for (int i = a; i <= b; i++) {
-            if (i % 3 == 0 || is369(i)) {
+            if (is369(i)) {
                 cnt++;
             }
         }
