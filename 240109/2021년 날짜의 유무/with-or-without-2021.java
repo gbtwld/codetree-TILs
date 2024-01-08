aa4bbe660c1a5af53a7d3ed74d752f9581b5bc8a
gbtwld <gbtwld@gmail.com>
2024-01-09 07:40:31 +0900

    Title: 2021년 날짜의 유무, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240109/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isExist(int m, int d) {
+        if (m == 2) {
+            return d <= 28;
+        } else if (m < 8) {
+            if (m % 2 == 0) {
+                return d <= 30;
+            } else {
+                return d <= 31;
+            }
+        } else {
+            if (m % 2 == 0) {
+                return d <= 31;
+            } else {
+                return d <= 30;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), d = sc.nextInt();
+
+        if (isExist(m, d)) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+}
