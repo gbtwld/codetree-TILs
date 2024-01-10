af079bfbe2216903f98e8860ce27ab93e0ef9f66
gbtwld <gbtwld@gmail.com>
2024-01-10 11:37:41 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int getCount(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        
+        if (n % 2 == 0) {
+            return getCount(n / 2) + 1;
+        } else {
+            return getCount(n * 3 + 1) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print(getCount(n));
+    }
+}
