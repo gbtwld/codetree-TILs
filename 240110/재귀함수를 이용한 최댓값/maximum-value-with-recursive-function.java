9c7fb43a0e11ef42db86daa5c3f4058e4176459c
gbtwld <gbtwld@gmail.com>
2024-01-10 11:19:35 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240110/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ 240110/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static int max(int[] arr, int n) {
+        if (n == 0) {
+            return arr[0];
+        }
         if (n == 1) {
             return Math.max(arr[1], arr[0]);
         }
