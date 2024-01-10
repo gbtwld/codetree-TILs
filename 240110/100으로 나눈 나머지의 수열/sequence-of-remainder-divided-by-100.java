6e7dfd38af2f6451bb2cbb2537e879795b06a9a3
gbtwld <gbtwld@gmail.com>
2024-01-10 11:39:49 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int getNum(int n) {
+        if (n == 1) {
+            return 2;
+        } else if (n == 2) {
+            return 4;
+        }
+
+        return getNum(n - 1) * getNum(n - 2) % 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print(getNum(n));
+    }
+}
