8191c2e6e6a3893dc4046db22bf87acf13debeea
gbtwld <gbtwld@gmail.com>
2024-01-11 11:00:33 +0900

    Title: 줄 세우기 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240111/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Student implements Comparable<Student> {
+    int h, w, number;
+
+    public Student(int h, int w, int number) {
+        this.h = h;
+        this.w = w;
+        this.number = number;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        if (this.h == student.h) {
+            return this.w - student.h;
+        }
+        return this.h - student.h;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] arr = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Student(sc.nextInt(), sc.nextInt(), i + 1);
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d %d %d\n", arr[i].h, arr[i].w, arr[i].number);
+        }
+    }
+}
