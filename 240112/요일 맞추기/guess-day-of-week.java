9725f6f54d126d4c8efcdb27a9b8710a01ae5dfb
gbtwld <gbtwld@gmail.com>
2024-01-12 19:45:54 +0900

    Title: 요일 맞추기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240112/요일 맞추기/guess-day-of-week.java
+++ 240112/요일 맞추기/guess-day-of-week.java
@@ -6,28 +6,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt(), d1 = sc.nextInt(), m2 = sc.nextInt(), d2 = sc.nextInt();
-        int elapsedDay = 0, resultIdx = 1;
+        int elapsedDay1 = 0, elapsedDay2 = 0, realElapsedDay = 0, resultIdx = 1;
         int[] date = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] day = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
-        while (true) {
-            if (m1 == m2 && d1 == d2) {
-                break;
-            }
-
-            elapsedDay++;
-            d1--;
-
-            if (d1 == 0) {
-                m1--;
-                d1 = date[m1];
-            }
+        for (int i = 1; i < m1; i++) {
+            elapsedDay1 += date[i];
         }
-        
-        for (int i = 0; i < elapsedDay; i++) {
-            resultIdx--;
-            if (resultIdx < 0) {
-                resultIdx = 6;
+        elapsedDay1 += d1;
+        for (int i = 1; i < m2; i++) {
+            elapsedDay2 += date[i];
+        }
+        elapsedDay2 += d2;
+
+        realElapsedDay = elapsedDay1 - elapsedDay2;
+
+        if (realElapsedDay < 0) {
+            for (int i = 0; i < Math.abs(realElapsedDay); i++) {
+                resultIdx++;
+                if (resultIdx > 6) {
+                    resultIdx = 0;
+                }
+            }
+        } else {
+            for (int i = 0; i < realElapsedDay; i++) {
+                resultIdx--;
+                if (resultIdx < 0) {
+                    resultIdx = 6;
+                }
             }
         }
 
