a87f44457f95ec8728067f0725e72c826436d645
gbtwld <gbtwld@gmail.com>
2024-01-13 13:54:32 +0900

    Title: 흰검 칠하기, Time: 224ms, Memory: 16MB, Status: Passed - CodeTree

--- 240113/흰검 칠하기/painting-white-black.java
+++ 240113/흰검 칠하기/painting-white-black.java
@@ -8,47 +8,43 @@ public class Main {
         int n = sc.nextInt(), curIdx = n * 100, wCnt = 0, kCnt = 0, gCnt = 0;
         int[] arrW = new int[n * 100 * 2];
         int[] arrK = new int[n * 100 * 2];
+        int[] arrG = new int[n * 100 * 2];
         
         for (int i = 0; i < n; i++) {
             int number = sc.nextInt();
             char dir = sc.next().charAt(0);
 
             // 회색되면 해당 타일 숫자 빼줘야댐
+            // 같은 색으로 다시 칠하는 경우 (number == 1)
 
             if (dir == 'L') {
                 for (int j = number - 1; j >= 0; j--) {
                     int targetIdx = curIdx - (number - (j + 1));
-                    arrW[targetIdx]++;
-                    if (arrW[targetIdx] >= 2 && arrK[targetIdx] >= 2) {
-                        kCnt--;
-                    } else {
-                        wCnt++;
-                        if (arrK[targetIdx] > 0) {
-                            kCnt--;
-                        }
-                    }
+
+                    arrW[targetIdx] = Math.abs(arrW[targetIdx]) + 1;
+                    arrK[targetIdx] *= -1;
                 }
+
                 curIdx -= number - 1;
             } else if (dir == 'R') {
                 for (int j = 0; j < number; j++) {
                     int targetIdx = curIdx + j;
-                    arrK[targetIdx]++;
-                    if (arrW[targetIdx] >= 2 && arrK[targetIdx] >= 2) {
-                        wCnt--;
-                    } else {
-                        kCnt++;
-                        if (arrW[targetIdx] > 0) {
-                            wCnt--;
-                        }
-                    }
+
+                    arrK[targetIdx] = Math.abs(arrK[targetIdx]) + 1;
+                    arrW[targetIdx] *= -1;
                 }
+
                 curIdx += number - 1;
             }
         }
 
         for (int i = 0; i < arrW.length; i++) {
-            if (arrW[i] >= 2 && arrK[i] >= 2) {
+            if (Math.abs(arrW[i]) >= 2 && Math.abs(arrK[i]) >= 2) {
                 gCnt++;
+            } else if (arrW[i] > arrK[i]) {
+                wCnt++;
+            } else if (arrK[i] > arrW[i]) {
+                kCnt++;
             }
         }
 
