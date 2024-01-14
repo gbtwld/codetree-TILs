b9228ce9c78a79d66a5642f2ba41947fcf0e04df
gbtwld <gbtwld@gmail.com>
2024-01-14 14:28:30 +0900

    Title: 벌금은 누구에게, Time: 238ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240114/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt(), result = 0;;
+        int[] arr = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            int num = sc.nextInt();
+            arr[num]++;
+            if (arr[num] == k) {
+                result = num;
+                break;
+            }
+        }
+
+        System.out.print(result);
+    }
+}
