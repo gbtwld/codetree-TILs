9a8c99b466d7d8898a79b0104cb19d1a2a741e11
gbtwld <gbtwld@gmail.com>
2024-01-14 13:31:16 +0900

    Title: 연속되는 수 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240114/연속되는 수 2/continuous-number2.java
+++ 240114/연속되는 수 2/continuous-number2.java
@@ -11,11 +11,14 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            if (i == 0 || arr[i] != arr[i - 1]) {
+            if (i == 0 || arr[i] != arr[i - 1] || i == n - 1) {
                 if (i == 0) {
                     cnt = 1;
                 }
                 if (cnt > maxCnt) {
+                    if (i == n - 1) {
+                        cnt++;
+                    }
                     maxCnt = cnt;
                 }
                 cnt = 1;
