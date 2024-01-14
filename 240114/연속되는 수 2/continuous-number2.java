edd20ba67a5679f146c8c428612a0c24313ba00d
gbtwld <gbtwld@gmail.com>
2024-01-14 13:34:46 +0900

    Title: 연속되는 수 2, Time: 190ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240114/연속되는 수 2/continuous-number2.java
+++ 240114/연속되는 수 2/continuous-number2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), cnt = 0, maxCnt = Integer.MIN_VALUE;
+        int n = sc.nextInt(), cnt = 1, maxCnt = Integer.MIN_VALUE;
         int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
@@ -11,19 +11,20 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            if (i == 0 || arr[i] != arr[i - 1] || i == n - 1) {
-                if (i == 0) {
-                    cnt = 1;
-                }
-                if (cnt > maxCnt) {
-                    if (i == n - 1) {
-                        cnt++;
-                    }
+            // i = 0 , i = n - 1
+            if (i == 0 || arr[i] != arr[i - 1]) {
+                if (maxCnt < cnt) {
                     maxCnt = cnt;
                 }
-                cnt = 1;
             } else {
-                cnt++;
+                if (i == n - 1) {
+                    cnt++;
+                    if (maxCnt < cnt) {
+                        maxCnt = cnt;
+                    }
+                } else {
+                    cnt++;
+                }
             }
         }
 
