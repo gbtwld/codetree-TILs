2da3370505ebf6aa54ab864a6b6d25515bfaa3be
gbtwld <gbtwld@gmail.com>
2024-01-14 14:05:56 +0900

    Title: 만나는 그 순간, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240114/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), curTime = 0, curPosition = 0, result = -1;
+        int[] a = new int[1000], b = new int[1000];
+
+        for (int i = 0; i < n; i++) {
+            char dir = sc.next().charAt(0);
+            int num = sc.nextInt();
+
+            if (dir == 'L') {
+                for (int j = 1; j <= num; j++) {
+                    curTime++;
+                    curPosition--;
+                    a[curTime] = curPosition;
+                }
+            } else {
+                for (int j = 1; j <= num; j++) {
+                    curTime++;
+                    curPosition++;
+                    a[curTime] = curPosition;
+                }
+            }
+        }
+        curTime = 0;
+        curPosition = 0;
+        for (int i = 0; i < m; i++) {
+            char dir = sc.next().charAt(0);
+            int num = sc.nextInt();
+
+            if (dir == 'L') {
+                for (int j = 1; j <= num; j++) {
+                    curTime++;
+                    curPosition--;
+                    b[curTime] = curPosition;
+                }
+            } else {
+                for (int j = 1; j <= num; j++) {
+                    curTime++;
+                    curPosition++;
+                    b[curTime] = curPosition;
+                }
+            }
+        }
+
+        for (int i = 1; i < Math.min(a.length, b.length); i++) {
+            if (a[i] == b[i]) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.print(result);
+    }
+}
