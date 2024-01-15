e267e884ff37d0a0a8d59b08bd45abc73a9f8f01
gbtwld <gbtwld@gmail.com>
2024-01-15 13:49:31 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240115/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), timeA = 0, timeB = 0, result = 0;
+        int[] arrA = new int[1000000], arrB = new int[1000000];
+
+        for (int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            if (d == 'L') {
+                for (int j = 1; j <= t; j++) {
+                    arrA[timeA + j] = arrA[timeA + j - 1] - 1;
+                }
+                timeA += t;
+            } else {
+                for (int j = 1; j <= t; j++) {
+                    arrA[timeA + j] = arrA[timeA + j - 1] + 1;
+                }
+                timeA += t;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            if (d == 'L') {
+                for (int j = 1; j <= t; j++) {
+                    arrB[timeB + j] = arrB[timeB + j - 1] - 1;
+                }
+                timeB += t;
+            } else {
+                for (int j = 1; j <= t; j++) {
+                    arrB[timeB + j] = arrB[timeB + j - 1] + 1;
+                }
+                timeB += t;
+            }
+        }
+
+        for (int i = 1; i < Math.max(timeA, timeB); i++) {
+            if (i > timeA) {
+                arrA[i] = arrA[timeA];
+            }
+            if (i > timeB) {
+                arrB[i] = arrB[timeB];
+            }
+            if (i > 1 && arrA[i - 1] != arrB[i - 1] && arrA[i] == arrB[i]) {
+                result++;
+            }
+        }
+
+        System.out.print(result);
+    }
+}
