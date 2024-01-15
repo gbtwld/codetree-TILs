041a6bf2f1efb661bffaa245339175d6125bea0b
gbtwld <gbtwld@gmail.com>
2024-01-15 15:19:12 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 362ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240115/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240115/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -36,13 +36,13 @@ public class Main {
             if (xVal > 1) {
                 timeArr[timeIdx][infoArr[i][1]] = xVal - 1;
                 if (yVal == 0) {
-                    timeArr[timeIdx][infoArr[i][2]] = 3;
+                    timeArr[timeIdx][infoArr[i][2]] = K;
                 }
             }
             if (yVal > 1) {
                 timeArr[timeIdx][infoArr[i][2]] = yVal - 1;
                 if (xVal == 0) {
-                    timeArr[timeIdx][infoArr[i][1]] = 3;
+                    timeArr[timeIdx][infoArr[i][1]] = K;
                 }
             }
         }
