432519c1cac0c508f3d4cfe31e3b1678f77a4f63
gbtwld <gbtwld@gmail.com>
2024-01-28 20:34:00 +0900

    Title: 체크판위에서 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240128/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt(), c = sc.nextInt(), result = 0;
+        char[][] arr = new char[r][c];
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        /*
+        1. 색이 다른 곳으로 이동해야함
+        2. 오른쪽, 아래쪽으로 한칸 이상 떨어진 곳으로 이동해야함
+        3. 시작, 도착 지점 제외 2곳만 지나야함
+        */
+
+        for (int i = 1; i < r - 1; i++) {
+            for (int j = 1; j < c - 1; j++) {
+                if (arr[i][j] != arr[0][0]) {
+                    for (int k = i + 1; k < r - 1; k++) {
+                        for (int l = j + 1; l < c - 1; l++) {
+                            if (arr[k][l] == arr[0][0]) {
+                                result++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.print(result);
+    }
+}
