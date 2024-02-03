5c5c6c12a437dbce8fe9c491083e98c116d0cab5
gbtwld <gbtwld@gmail.com>
2024-02-04 04:17:02 +0900

    Title: Carry 피하기 2, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240204/Carry 피하기 2/escaping-carry-2.java
+++ 240204/Carry 피하기 2/escaping-carry-2.java
@@ -2,26 +2,20 @@ import java.util.Scanner;
 
 // 서로 다른 3개의 숫자. 각 자리를 더했을 때 10을 넘지 않아야 함. 합의 최댓값 계산
 public class Main {
-    public static boolean checkCarry(int a, int b) {
+    public static boolean checkCarry(int a, int b, int c) {
         boolean satisfied = true;
         
-        while (a > 0 && b > 0) {
-            if (a % 10 + b % 10 >= 10) {
+        while (a > 0 || b > 0 || c > 0) {
+            if (a % 10 + b % 10 + c % 10 >= 10) {
                 satisfied = false;
                 break;
             }
             a /= 10;
             b /= 10;
+            c /= 10;
         }
 
         return satisfied;
-    }
-
-    public static boolean check(int a, int b, int c) {
-        // (a, b) (a, c) (b, c) 체크
-        if (checkCarry(a, b) && checkCarry(a, c) && checkCarry(b, c)) return true;
-
-        return false;
     }
 
     public static void main(String[] args) {
@@ -36,7 +30,7 @@ public class Main {
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    if (check(arr[i], arr[j], arr[k])) {
+                    if (checkCarry(arr[i], arr[j], arr[k])) {
                         result = Math.max(result, arr[i] + arr[j] + arr[k]);
                     }
                 }
