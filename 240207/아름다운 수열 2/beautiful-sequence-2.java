a5167b9574f676738e8a62e5517b836cc7f69e28
gbtwld <gbtwld@gmail.com>
2024-02-07 19:57:36 +0900

    Title: 아름다운 수열 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240207/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static boolean isBeautiful(int a, int b, int c, int[] B) {
+        int[] sortedArr = new int[]{a, b, c};
+        Arrays.sort(sortedArr);
+        
+        for (int i = 0; i < B.length; i++) {
+            if (sortedArr[i] != B[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), cnt = 0;
+        int[] arrA = new int[n], arrB = new int[m];
+        Arrays.sort(arrB);
+
+        for (int i = 0; i < n; i++) {
+            arrA[i] = sc.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            arrB[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - m + 1; i++) {
+            if (isBeautiful(arrA[i], arrA[i + 1], arrA[i + 2], arrB)) {
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
