338ca69a099b0b0195766fb8bb051e55424516f5
gbtwld <gbtwld@gmail.com>
2024-02-14 18:14:06 +0900

    Title: 개발팀의 능력, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240214/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isPossible = false;
+        int sum = 0, result = Integer.MAX_VALUE;
+        int[] arr = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                for (int k = 0; k < 4; k++) {
+                    for (int l = k + 1; k < 5; k++) {
+                        if (k == i || k == j || l == i || l == j) continue;
+                        if (arr[i] == arr[j] || arr[k] == arr[l]) continue;
+                        isPossible = true;
+
+                        int a = arr[i] + arr[j];
+                        int b = arr[k] + arr[l];
+                        int c = sum - (a + b);
+
+                        int maxValue = Math.max(a, Math.max(b, c));
+                        int minValue = Math.min(a, Math.min(b, c));
+                        int diff = maxValue - minValue;
+
+                        result = Math.min(result, diff);
+                    }
+                }
+            }
+        }
+
+        if (!isPossible) {
+            System.out.print(-1);
+        } else {
+            System.out.print(result);
+        }
+    }
+}
