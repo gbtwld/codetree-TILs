d6afb789e3312a1c977ec2a581489989b7865827
gbtwld <gbtwld@gmail.com>
2024-02-16 19:59:15 +0900

    Title: 그래프 탐색, Time: 475ms, Memory: 28MB, Status: Passed - CodeTree

--- 240216/그래프 탐색/graph-traversal.java
+++ 240216/그래프 탐색/graph-traversal.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int dfs(int vertex, int[][] graph, boolean[][] visited) {
+    public static int result = 0;
+
+    public static void DFS(int vertex, int[][] graph, boolean[] visited) {
         for (int i = 1; i < graph[0].length; i++) {
-            if (graph[vertex][i] == 1 && !visited[vertex][i]) {
-                visited[vertex][i] = true;
-                return dfs(i, graph, visited) + 1;
+            if (graph[vertex][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                result++;
+                DFS(i, graph, visited);
             }
         }
-        return 0;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt(), result = 0;
+        int n = sc.nextInt(), m = sc.nextInt();
         int[][] graph = new int[n][n];
-        boolean[][] visited = new boolean[n][n];
+        boolean[] visited = new boolean[n];
 
         for (int i = 0; i < m; i++) {
             int x = sc.nextInt() - 1, y = sc.nextInt() - 1;
             graph[x][y] = graph[y][x] = 1;
         }
 
-        System.out.print(dfs(0, graph, visited) - 1);
+        DFS(0, graph, visited);
+        System.out.print(result);
     }
 }
