b5a49e0464ab2d8d351bf548e51a38ffcb7f6cce
gbtwld <gbtwld@gmail.com>
2024-04-29 15:59:25 +0900

    Title: 팀으로 하는 틱택토 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240429/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
+++ 240429/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -12,6 +12,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int cnt = 0;
         int[][] arr = new int[3][3];
+        List<HashSet<Integer>> visited = new ArrayList<>();
 
         for (int i = 0; i < 3; i++) {
             int num = sc.nextInt();
@@ -24,18 +25,54 @@ public class Main {
 
         for (int i = 0; i < 3; i++) {
             if (checkTwo(arr[i][0], arr[i][1], arr[i][2])) {
-                cnt++;
+                HashSet<Integer> hashSet = new HashSet<>();
+                hashSet.add(arr[i][0]);
+                hashSet.add(arr[i][1]);
+                hashSet.add(arr[i][2]);
+                List<Integer> k = new ArrayList<>(hashSet);
+                Collections.sort(k);
+                if (!visited.contains(k)) {
+                    cnt++;
+                    visited.add(hashSet);
+                }
             }
             if (checkTwo(arr[0][i], arr[1][i], arr[2][i])) {
-                cnt++;
+                HashSet<Integer> hashSet = new HashSet<>();
+                hashSet.add(arr[0][i]);
+                hashSet.add(arr[1][i]);
+                hashSet.add(arr[2][i]);
+                List<Integer> k = new ArrayList<>(hashSet);
+                Collections.sort(k);
+                if (!visited.contains(k)) {
+                    cnt++;
+                    visited.add(hashSet);
+                }
             }
         }
 
         if (checkTwo(arr[0][0], arr[1][1], arr[2][2])) {
-            cnt++;
+            HashSet<Integer> hashSet = new HashSet<>();
+            hashSet.add(arr[0][0]);
+            hashSet.add(arr[1][1]);
+            hashSet.add(arr[2][2]);
+            List<Integer> k = new ArrayList<>(hashSet);
+            Collections.sort(k);
+            if (!visited.contains(k)) {
+                cnt++;
+                visited.add(hashSet);
+            }
         }
         if (checkTwo(arr[0][2], arr[1][1], arr[2][0])) {
-            cnt++;
+            HashSet<Integer> hashSet = new HashSet<>();
+            hashSet.add(arr[0][2]);
+            hashSet.add(arr[1][1]);
+            hashSet.add(arr[2][0]);
+            List<Integer> k = new ArrayList<>(hashSet);
+            Collections.sort(k);
+            if (!visited.contains(k)) {
+                cnt++;
+                visited.add(hashSet);
+            }
         }
 
         System.out.println(cnt);
