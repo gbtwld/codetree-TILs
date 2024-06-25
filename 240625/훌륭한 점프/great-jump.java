43e5ffe1c8264bb943b48e4cd5f3864ba9ff2e5e
gbtwld <gbtwld@gmail.com>
2024-06-25 17:14:42 +0900

    Title: 훌륭한 점프, Time: 105ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240625/훌륭한 점프/great-jump.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static boolean isPossible(int a, int[] arr, int k) {
+        int dist = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= a) {
+                if (dist > k) {
+                    return false;
+                }
+                dist = 1;
+            } else {
+                dist++;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = Math.max(arr[0], arr[n - 1]); i <= n; i++) {
+            if (isPossible(i, arr, k)) {
+                sb.append(i);
+                break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
