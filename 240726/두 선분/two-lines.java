08f7ad772417bd0aa4bcde42075a88e2f566436f
gbtwld <gbtwld@gmail.com>
2024-07-26 14:34:36 +0900

    Title: 두 선분, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240726/두 선분/two-lines.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int x3 = Integer.parseInt(st.nextToken());
+        int x4 = Integer.parseInt(st.nextToken());
+
+        if (x2 < x3 || x4 < x1) {
+            sb.append("nonintersecting");
+        } else {
+            sb.append("intersecting");
+        }
+
+        System.out.print(sb);
+    }
+}
