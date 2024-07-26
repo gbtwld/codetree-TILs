0f3add98fc66aa31551703905c8f64c947d7afac
gbtwld <gbtwld@gmail.com>
2024-07-26 17:05:04 +0900

    Title: 전부 겹치는 선분, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240726/전부 겹치는 선분/overlapping-line-segments.java
+++ 240726/전부 겹치는 선분/overlapping-line-segments.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
         int n = Integer.parseInt(br.readLine());
         
@@ -14,20 +15,19 @@ public class Main {
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= 100; i++) {
-            boolean satisfied = true;
-            for (int j = 0; j < n; j++) {
-                if (i < arr[j][0] || i > arr[j][1]) {
-                    satisfied = false;
-                    break;
-                }
-            }
-            if (satisfied) {
-                System.out.println("Yes");
-                return;
-            }
+        int minR = Integer.MAX_VALUE;
+        int maxL = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            minR = Math.min(minR, arr[i][1]);
+            maxL = Math.max(maxL, arr[i][0]);
         }
-        System.out.println("No");
-        return;
+
+        if (minR < maxL) {
+            sb.append("No");
+        } else {
+            sb.append("Yes");
+        }
+
+        System.out.print(sb);
     }
 }
